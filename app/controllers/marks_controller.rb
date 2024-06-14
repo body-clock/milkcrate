073c757fbee0115fe2aabb04a0ce06cf92adc817
Patrick Perkins <lilblizzard97@gmail.com>
2024-06-14 18:01:29 -0400

    use rails requestjs library to make the request, turbo stream appends when mark is created

--- app/controllers/marks_controller.rb
+++ app/controllers/marks_controller.rb
@@ -8,7 +8,12 @@ class MarksController < ApplicationController
     @record = Record.find(params[:record_id])
     @mark = @record.marks.build(mark_params)
     if @mark.save
-      render json: { success: true, mark: @mark }, status: :created
+      respond_to do |format|
+        format.turbo_stream
+        format.html do
+          redirect_to crate_record_path(@record.crate.id, @record.id), notice: 'Mark was successfully created.'
+        end
+      end
     else
       render json: { success: false, errors: @mark.errors.full_messages }, status: :unprocessable_entity
     end
