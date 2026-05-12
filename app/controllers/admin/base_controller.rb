fb9dab6a24ef56a16a34a6274ef1a2f8a6e141cf
Patrick Perkins <pperkins@upenn.edu>
2026-05-12 10:46:21 -0400

    base controller auth fix

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,8 +1,17 @@
 class Admin::BaseController < ApplicationController
-  creds_user = ENV["ADMIN_HTTP_BASIC_USER"].presence || Rails.application.credentials.dig(:admin, :http_basic_auth_user)
-  creds_pass = ENV["ADMIN_HTTP_BASIC_PASSWORD"].presence || Rails.application.credentials.dig(:admin, :http_basic_auth_password)
+  before_action :http_basic_auth_admin
 
-  if creds_user && creds_pass
-    http_basic_authenticate_with(name: creds_user, password: creds_pass)
+  private
+
+  def http_basic_auth_admin
+    creds_user = ENV["ADMIN_HTTP_BASIC_USER"].presence || Rails.application.credentials.dig(:admin, :http_basic_auth_user)
+    creds_pass = ENV["ADMIN_HTTP_BASIC_PASSWORD"].presence || Rails.application.credentials.dig(:admin, :http_basic_auth_password)
+
+    return unless creds_user && creds_pass
+
+    authenticate_or_request_with_http_basic("Admin") do |username, password|
+      ActiveSupport::SecurityUtils.secure_compare(username, creds_user) &
+        ActiveSupport::SecurityUtils.secure_compare(password, creds_pass)
+    end
   end
 end
